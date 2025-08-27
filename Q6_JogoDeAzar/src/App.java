806b957a9fe623411bd1cc279ff7d3b1e3eafbe1
PedroSup <137943366+pedrosoup27@users.noreply.github.com>
2025-08-27 19:59:26 +0000

    Dado com histórico implementado

--- Q6_JogoDeAzar/src/App.java
+++ Q6_JogoDeAzar/src/App.java
@@ -2,9 +2,21 @@ public class App {
     public static void main(String[] args) {
         Dado d6 = new Dado(6);
         Dado d20 = new Dado(20);
-        d6.rolar();
-        d20.rolar();
-        System.out.println("D6: " + d6.getValor());
-        System.out.println("D20: " + d20.getValor());
+
+        DadoHistorico d6Hist = new DadoHistorico(d6);
+        DadoHistorico d20Hist = new DadoHistorico(d20);
+
+        //Roda uma vez
+        d6Hist.rolar();
+        d20Hist.rolar();
+        //Rola duas
+        d6Hist.rolar();
+        d20Hist.rolar();
+        //Rola três
+        d6Hist.rolar();
+        d20Hist.rolar();
+
+        System.out.println("D6: " + d6Hist.getValor() + " Histórico: " + d6Hist.getHistorico());
+        System.out.println("D20: " + d20Hist.getValor() + " Histórico: " + d20Hist.getHistorico());
     }
 }
--- Q6_JogoDeAzar/src/DadoHistorico.java
+++ Q6_JogoDeAzar/src/DadoHistorico.java
@@ -4,7 +4,24 @@ import java.util.List;
 public class DadoHistorico extends DadoDecorator{
     private List<Integer> historico = new ArrayList<>();
 
-    
+    public DadoHistorico(IDado dado) {
+        super(dado);
+    }
+
+    @Override
+    public void rolar() {
+        super.rolar();
+        historico.add(dado.getValor());
+    }
+
+    @Override
+    public int getValor() {
+        return dado.getValor();
+    }
+
+    public List<Integer> getHistorico() {
+        return historico;
+    }
 
 
 }
